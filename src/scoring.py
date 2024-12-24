fd7302208e0777ff90fa81ba68ef1c3365a380dd
trifonovtema <artyom85@gmail.com>
2024-12-25 04:25:24 +0700

    add store to scoring

--- src/scoring.py
+++ src/scoring.py
@@ -1,39 +1,45 @@
-import random
+import hashlib
+import json
+from src.scoring_requests import OnlineScoreRequest
 
 
 def get_score(
     store,
-    phone=None,
-    email=None,
-    birthday=None,
-    gender=None,
-    first_name=None,
-    last_name=None,
-):
-    score = 0
-    if phone:
+    online_score_request: OnlineScoreRequest,
+) -> float:
+    key_parts = [
+        online_score_request.first_name or "",
+        online_score_request.last_name or "",
+        online_score_request.phone or "",
+        (
+            online_score_request.birthday.strftime("%Y%m%d")
+            if online_score_request.birthday
+            else ""
+        ),
+    ]
+    key = "uid:" + hashlib.md5("".join(key_parts).encode("utf-8")).hexdigest()
+
+    # Try to get from cache
+    score = store.cache_get(key)
+    if score is not None:
+        return float(score)
+
+    # Calculate score
+    score = 0.0
+    if online_score_request.phone:
         score += 1.5
-    if email:
+    if online_score_request.email:
         score += 1.5
-    if birthday and gender:
+    if online_score_request.birthday and online_score_request.gender is not None:
         score += 1.5
-    if first_name and last_name:
+    if online_score_request.first_name and online_score_request.last_name:
         score += 0.5
+
+    # Cache the score for 60 minutes
+    store.cache_set(key, score, 60 * 60)
     return score
 
 
-def get_interests(store, cid):
-    interests = [
-        "cars",
-        "pets",
-        "travel",
-        "hi-tech",
-        "sport",
-        "music",
-        "books",
-        "tv",
-        "cinema",
-        "geek",
-        "otus",
-    ]
-    return random.sample(interests, 2)
+def get_interests(store, cid: str) -> list:
+    r = store.get(f"i:{cid}")
+    return json.loads(r) if r else []
